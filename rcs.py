f2f3c037c56a3481f7f60ee9cf941d23aa1c58c5
RinCynar <0rhendx@gmail.com>
2024-06-26 18:03:22 +0800

    1.70

--- rcs.py
+++ rcs.py
@@ -10,7 +10,7 @@ RCS_FOLDER = ".rcs"
 KEY_FILE_TEMPLATE = os.path.join(RCS_FOLDER, "{}.rcs_keys")
 HISTORY_FILE_TEMPLATE = os.path.join(RCS_FOLDER, "{}.rcs_hst")
 OPT_FILE = "rcs_opt.md"
-RCS_VER = 1.69-fx0
+RCS_VER = 1.70
 DOWNLOAD_LINK = "https://rcva.san.tc/assets/file/rcs.py"
 UPDATE_URL = "http://rcva.san.tc/assets/rcs.html"
 
