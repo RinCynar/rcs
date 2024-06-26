86e3ca782c1f06ef4542d79c2ed00b17cbcb050b
RinCynar <0rhendx@gmail.com>
2024-06-26 15:57:42 +0800

    1.67

--- rcs.py
+++ rcs.py
@@ -6,45 +6,84 @@ from arc4 import ARC4
 
 DEFAULT_KEY = "DEF-4164E792FC9AD1C9C866B3D6DCC79A27"
 KEYS = [DEFAULT_KEY]
-KEY_FILE = ".rcs_keys"
-HISTORY_FILE = ".rcs_hst"
+RCS_FOLDER = ".rcs"
+KEY_FILE_TEMPLATE = os.path.join(RCS_FOLDER, "{}.rcs_keys")
+HISTORY_FILE_TEMPLATE = os.path.join(RCS_FOLDER, "{}.rcs_hst")
 OPT_FILE = "rcs_opt.md"
-RCS_VER = 1.59
+RCS_VER = 1.67
 DOWNLOAD_LINK = "https://rcva.san.tc/assets/file/rcs.py"
 UPDATE_URL = "http://rcva.san.tc/assets/rcs.html"
 
+username = ""
+
+
 def print_message(message):
-    print("")
-    print(message)
-    print("")
+    print("\n" + message + "\n")
+
 
 def get_input(prompt, default=None):
     user_input = input(prompt).strip()
     return user_input if user_input else default
 
+
 def load_keys():
-    global KEYS
+    global KEYS, username
+    key_file = KEY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
     try:
-        with open(KEY_FILE, "r") as file:
-            KEYS = [DEFAULT_KEY] + [line.strip() for line in file.readlines() if line.strip() != DEFAULT_KEY]
+        with open(key_file, "rb") as file:
+            encrypted_keys = file.readlines()
+            for line in encrypted_keys:
+                decrypted_line = rc4_decrypt(
+                    username.encode("utf-16"), line.strip()
+                ).decode("utf-16")
+                if decrypted_line.strip() != DEFAULT_KEY:
+                    KEYS.append(decrypted_line.strip())
     except FileNotFoundError:
         KEYS = [DEFAULT_KEY]
 
+
 def save_keys():
-    with open(KEY_FILE, "w") as file:
+    key_file = KEY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
+    with open(key_file, "wb") as file:
+        encrypted_username = (
+            rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")) + b"\n"
+        )
+        file.write(encrypted_username)
         for key in KEYS:
             if key != DEFAULT_KEY:
-                file.write(key + "\n")
+                encrypted_key = (
+                    rc4_encrypt(username.encode("utf-16"), key.encode("utf-16")) + b"\n"
+                )
+                file.write(encrypted_key)
 
-def reset_keys():
+
+def reset():
     global KEYS
     try:
-        os.remove(KEY_FILE)
+        os.remove(
+            HISTORY_FILE_TEMPLATE.format(
+                bytes_to_hex(
+                    rc4_encrypt(username.encode("utf-16"), username.encode("utf-16"))
+                )
+            )
+        )
+        os.remove(
+            KEY_FILE_TEMPLATE.format(
+                bytes_to_hex(
+                    rc4_encrypt(username.encode("utf-16"), username.encode("utf-16"))
+                )
+            )
+        )
     except FileNotFoundError:
         pass
     KEYS = [DEFAULT_KEY]
     save_keys()
     print_message("Restoring default configuration completed.")
+
 
 def add_key(new_key):
     global KEYS
@@ -54,6 +93,7 @@ def add_key(new_key):
         print_message(f"Key added: {new_key}")
     else:
         print_message(f"Key '{new_key}' already exists.")
+
 
 def delete_key(key_number):
     global KEYS
@@ -71,24 +111,30 @@ def delete_key(key_number):
     except ValueError:
         print_message(f"Invalid key number: {key_number}")
 
-def utf16be_to_bytes(s):
-    return s.encode('utf-16be')
+
+def utf16_to_bytes(s):
+    return s.encode("utf-16")
+
 
 def rc4_encrypt(key, plaintext):
     cipher = ARC4(key)
     ciphertext = cipher.encrypt(plaintext)
     return ciphertext
 
+
 def rc4_decrypt(key, ciphertext):
     cipher = ARC4(key)
     plaintext = cipher.decrypt(ciphertext)
     return plaintext
 
+
 def bytes_to_hex(b):
     return b.hex().upper()
 
+
 def hex_to_bytes(h):
     return bytes.fromhex(h)
+
 
 def choose_key_for_encryption():
     global KEYS
@@ -107,6 +153,7 @@ def choose_key_for_encryption():
         print_message("Invalid choice, using default key.")
         return KEYS[0]
 
+
 def choose_key_for_decryption():
     global KEYS
     print_message("Trying keys in order:")
@@ -114,29 +161,49 @@ def choose_key_for_decryption():
         print(f"{i}-{key[:3]}")
     return KEYS
 
+
 def save_history(record):
-    with open(HISTORY_FILE, "a") as file:
-        file.write(record + "\n")
+    encrypted_record = rc4_encrypt(username.encode("utf-16"), record.encode("utf-16"))
+    history_file = HISTORY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
+    with open(history_file, "ab") as file:
+        file.write(encrypted_record + b"\n")
+
 
 def display_history():
+    history_file = HISTORY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
     try:
-        with open(HISTORY_FILE, "r") as file:
+        with open(history_file, "rb") as file:
             history = file.readlines()
             if not history:
                 print_message("No history records found.")
             else:
                 for line in history:
-                    print(line.strip())
-                    print("")
+                    try:
+                        decrypted_line = rc4_decrypt(
+                            username.encode("utf-16"), line.strip()
+                        )
+                        print(decrypted_line.decode("utf-16").rstrip("\x00"))
+                        print("")
+                    except Exception as e:
+                        print_message(f"Error decrypting history record: {str(e)}")
     except FileNotFoundError:
         print_message("No history records found.")
 
+
 def clear_history():
+    history_file = HISTORY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
     try:
-        os.remove(HISTORY_FILE)
+        os.remove(history_file)
         print_message("History records cleared.")
     except FileNotFoundError:
         print_message("No history records to clear.")
+
 
 def check_for_updates():
     try:
@@ -147,56 +214,93 @@ def check_for_updates():
     except requests.RequestException:
         return None
 
+
 def handle_command(user_input):
-    if user_input.lower() == 'rcs-exi':
+    global username
+    if user_input.lower() == "rcs-exi":
         return False
-    elif user_input.lower() == 'rcs-help':
+    elif user_input.lower() == "rcs-help":
         print_help()
-    elif user_input.startswith('rcs-adk'):
-        new_key = user_input.split(' ', 1)[1]
+    elif user_input.startswith("rcs-adk"):
+        new_key = user_input.split(" ", 1)[1]
         add_key(new_key)
-    elif user_input.startswith('rcs-dek'):
+    elif user_input.startswith("rcs-dek"):
         parts = user_input.split()
-        if len(parts) == 2 and parts[0] == 'rcs-dek' and parts[1].startswith('-'):
+        if len(parts) == 2 and parts[0] == "rcs-dek" and parts[1].startswith("-"):
             key_number = parts[1][1:]
             delete_key(key_number)
         else:
-            print_message("Invalid input format for rcs-dek command. Format should be: rcs-dek -<key_number>")
-    elif user_input.lower() == 'rcs-res':
-        reset_keys()
-    elif user_input.lower() == 'rcs-cuk':
+            print_message(
+                "Invalid input format for rcs-dek command. Format should be: rcs-dek -<key_number>"
+            )
+    elif user_input.lower() == "rcs-res":
+        reset()
+    elif user_input.lower() == "rcs-cuk":
         display_keys()
         print("")
-    elif user_input.startswith('rcs-pod'):
-        text_to_crack = user_input.split(' ', 1)[1]
+    elif user_input.startswith("rcs-pod"):
+        text_to_crack = user_input.split(" ", 1)[1]
         bruteforce_decrypt(text_to_crack)
-    elif user_input.lower() == 'rcs-hst':
+    elif user_input.lower() == "rcs-hst":
         display_history()
-    elif user_input.lower() == 'rcs-cle':
+    elif user_input.lower() == "rcs-cle":
         clear_history()
-    elif user_input.lower() == 'rcs-udt':
+    elif user_input.lower() == "rcs-udt":
         latest_version = check_for_updates()
         if latest_version:
             if latest_version > RCS_VER:
-                print_message(f"New version {latest_version} is available! Download it here: {DOWNLOAD_LINK}")
+                print_message(
+                    f"New version {latest_version} is available! Download it here: {DOWNLOAD_LINK}"
+                )
             else:
                 print_message("You are using the latest version.")
         else:
             print_message("Failed to check for updates.")
-    elif user_input.startswith('- '):
+    elif user_input.startswith("- "):
         decrypt_text(user_input)
     else:
         encrypt_text(user_input)
     return True
 
+
 def interactive_mode():
-    print_message(f"rcs {RCS_VER}, a text encryption tool based on RC4 encryption algorithm\nhttp://rcva.san.tc, Rin' Cynar\nType 'rcs-help' for usage instructions")
+    global username
+
+    print_message(
+        f"rcs {RCS_VER}, a text encryption tool based on RC4 encryption algorithm\nhttp://rcva.san.tc, Rin' Cynar\nType 'rcs-help' for usage instructions"
+    )
+
+    username = get_input("Enter your username: ")
+    key_file = KEY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
+    history_file = HISTORY_FILE_TEMPLATE.format(
+        bytes_to_hex(rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")))
+    )
+
+    if not os.path.exists(RCS_FOLDER):
+        os.mkdir(RCS_FOLDER)
+
+    if not os.path.exists(key_file):
+        encrypted_username = (
+            rc4_encrypt(username.encode("utf-16"), username.encode("utf-16")) + b"\n"
+        )
+        with open(key_file, "wb") as file:
+            file.write(encrypted_username)
+
+    load_keys()
+    if not os.path.exists(history_file):
+        open(history_file, "wb").close()
+        print(f"\nUser: {username} created")
+        print(f"Enter as {username}\n")
 
     latest_version = check_for_updates()
     if latest_version:
         if latest_version > RCS_VER:
-            print_message(f"A new version {latest_version} is available! Download it here: {DOWNLOAD_LINK}")
-    
+            print_message(
+                f"A new version {latest_version} is available! Download it here: {DOWNLOAD_LINK}"
+            )
+
     while True:
         try:
             user_input = input("# ").strip()
@@ -205,17 +309,23 @@ def interactive_mode():
         except Exception as e:
             print_message(f"Error: {str(e)}")
 
+
 def print_help():
-    print_message("Provide the text and press 'Enter', rcs will automatically perform the encryption work, you can choose the key to use for encryption, or just simply press 'Enter' again to use the default options.\nEnter '- <text> -<key_number>' and press Enter, rcs will use the key you specified to decrypt. Of course, you can choose to simply enter '- <text>', rcs will try all the keys that have been saved and return the results.\nType 'rcs-adk <new-key>' to add a new encryption key.\nType 'rcs-cle' to clear encryption/decryption history.\nType 'rcs-cuk' to display the currently saved encryption keys\nType 'rcs-dek -<key_number>' to delete a specified encryption key.\nType 'rcs-exi' to exit.\nType 'rcs-hst' to display encryption/decryption history.\nType 'rcs-pod <text>' to perform a brute force decryption on the specified text.\nType 'rcs-res' to reset default configuration.\nType 'rcs-udt' to check for updates.")
+    print_message(
+        "Provide the text and press 'Enter', rcs will automatically perform the encryption work, you can choose the key to use for encryption, or just simply press 'Enter' again to use the default options.\nEnter '- <text> -<key_number>' and press Enter, rcs will use the key you specified to decrypt. Of course, you can choose to simply enter '- <text>', rcs will try all the keys that have been saved and return the results.\nType 'rcs-adk <new-key>' to add a new encryption key.\nType 'rcs-cle' to clear encryption/decryption history.\nType 'rcs-cuk' to display the currently saved encryption keys\nType 'rcs-dek -<key_number>' to delete a specified encryption key.\nType 'rcs-exi' to exit.\nType 'rcs-hst' to display encryption/decryption history.\nType 'rcs-pod <text>' to perform a brute force decryption on the specified text.\nType 'rcs-res' to reset default configuration.\nType 'rcs-udt' to check for updates."
+    )
+
 
 def display_keys():
+    global username
     print_message("Current keys:")
     for i, key in enumerate(KEYS):
-        print(f"{i}-{key[:3]}x.")
+        print(f"{i}-{key[:3]}")
+
 
 def decrypt_text(user_input):
-    global KEYS
-    parts = user_input.split(' ')
+    global KEYS, username
+    parts = user_input.split(" ")
     if len(parts) < 2:
         print_message("Invalid input format.")
         return
@@ -237,10 +347,12 @@ def decrypt_text(user_input):
 
     for key in keys_to_try:
         try:
-            key_bytes = utf16be_to_bytes(key)
+            key_bytes = utf16_to_bytes(key)
             plaintext_bytes = rc4_decrypt(key_bytes, ciphertext_bytes)
-            decrypted_text = plaintext_bytes.decode('utf-16be')
-            decryption_results.append(f"Decrypted text with key {key[:3]}: {decrypted_text}")
+            decrypted_text = plaintext_bytes.decode("utf-16")
+            decryption_results.append(
+                f"Decrypted text with key {key[:3]}: {decrypted_text}"
+            )
         except Exception as e:
             decryption_results.append(f"Decryption failed with key {key[:3]}")
             continue
@@ -249,17 +361,21 @@ def decrypt_text(user_input):
         print_message(result)
         save_history(result)
 
+
 def encrypt_text(plaintext):
+    global KEYS, username
     key = choose_key_for_encryption()
-    key_bytes = utf16be_to_bytes(key)
-    plaintext_bytes = utf16be_to_bytes(plaintext)
+    key_bytes = utf16_to_bytes(key)
+    plaintext_bytes = utf16_to_bytes(plaintext)
     ciphertext_bytes = rc4_encrypt(key_bytes, plaintext_bytes)
     ciphertext_hex = bytes_to_hex(ciphertext_bytes)
     print_message(f"Encrypted text: {ciphertext_hex}")
     save_history(f"Encrypted text: {ciphertext_hex} with key {key[:3]}")
 
+
 def bruteforce_decrypt(ciphertext):
-    character_set = "`~!@#$%^&*()-=_+[]\\{}|;':"",./<>?0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    global username
+    character_set = "`~!@#$%^&*()-=_+[]\\{}|;':\",./<>?0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     min_length = int(input("Enter minimum key length: "))
     max_length = int(input("Enter maximum key length: "))
 
@@ -267,16 +383,18 @@ def bruteforce_decrypt(ciphertext):
         for length in range(min_length, max_length + 1):
             print(f"Trying keys of length {length}...")
             for attempt in itertools.product(character_set, repeat=length):
-                key = ''.join(attempt)
+                key = "".join(attempt)
                 try:
-                    decrypted_text = rc4_decrypt(utf16be_to_bytes(key), hex_to_bytes(ciphertext))
-                    decrypted_text = decrypted_text.decode('utf-16be').rstrip('\x00')
+                    decrypted_text = rc4_decrypt(
+                        utf16_to_bytes(key), hex_to_bytes(ciphertext)
+                    )
+                    decrypted_text = decrypted_text.decode("utf-16").rstrip("\x00")
                     output_file.write(f"Key: {key}, Decrypted text: {decrypted_text}\n")
                 except Exception as e:
                     continue
 
     print("Bruteforce decryption completed. Results saved in rcs_opt.md\n")
-    
+
+
 if __name__ == "__main__":
-    load_keys()
     interactive_mode()
